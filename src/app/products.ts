a3b31c7e6849c25493198fb82912ec3ea3876f29
Alex Cotelin <alex.cotelin@gmail.com>
2021-03-09 15:36:52 +0000

    Add translation config, tag translations in template, extract and translate

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,23 +2,22 @@ export const products = [
   {
     id: 1,
     name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    price: 1799,
+    description: 'A large phone with one of the best screens',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
     name: 'Phone Standard',
-    price: 299,
-    description: ''
-  }
+    price: 299.5,
+    description: '',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
