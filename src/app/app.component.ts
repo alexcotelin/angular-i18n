01bcc55c7599a5d886d56705657e2051e6c0ae15
Alex Cotelin <alex.cotelin@gmail.com>
2021-03-28 17:48:26 +0100

    Tag template literal example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // pageIntro = $localize`Hello Alex!`
+  userName = 'Alex';
+  //                    :<meaning>  | <description>      <@@id>:
+  pageIntro = $localize`:Site header|Greeting the customer@@siteHeader:Hello ${this.userName}!`;
+  pageIntro2 = $localize`Hello ${this.userName}:userFirstName:!`;
+
+  constructor() {
+    this.greet`Hello, ${this.userName}!`;
+  }
+
+  /**
+   * Tagged template example
+   * @param textArray
+   * @param PH1
+   */
+  greet(textArray: TemplateStringsArray, PH1: string | number) {
+    //Hello, ,!<>Alex
+    console.log(textArray + '<>' + PH1);
+    //Hello, <>!<>undefined<>Alex
+    console.log(`${textArray[0]}<>${textArray[1]}<>${textArray[2]}<>${PH1}`);
+  }
 }
-
-
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,6 +9,7 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  currentDate = new Date();
   items = this.cartService.getItems();
   checkoutForm = this.formBuilder.group({
     name: '',
